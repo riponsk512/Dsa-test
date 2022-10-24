d3f9c23b1e81aa22c2d8bab1a1a0481463ccbf3a
Ripon Sk <riponsk477@gmail.com>
2022-10-24 22:02:41 +0530

    Find the element that appears once in a sorted array

--- Dsa test/main.swift
+++ Dsa test/main.swift
@@ -6,6 +6,20 @@
 //
 
 import Foundation
+fileprivate class DivideAndConqure{
+    func Search(arr:[Int],len:Int){
+        var num = 0;
+        for i in stride(from: 0, through: len-1, by: 1){
+            num = num^arr[i]
+           
+            
+        }
+        print("Required Elemt for arr is:\(num)")
+    }
+}
+var arr = [1, 1, 2, 4, 4, 5, 5, 6, 6]
+fileprivate var div = DivideAndConqure()
+div.Search(arr: arr, len: arr.count)
 
-print("Hello, World!")
 
+print(4^6)
